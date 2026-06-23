cac59f1717544a619dc282e4dee6028b3570a44f
lleniks <elena160108lena@gmail.com>
2026-06-24 00:53:47 +0500

    фикс путей фронт

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(cors());
 app.use(express.json());
 
-const frontendPath = path.resolve(__dirname, '../frontend');
-app.use(express.static(frontendPath));
+const frontendPath
+app.use(express.static(frontendPath)); = path.resolve(__dirname, '../frontend');
 
-app.use('/css', express.static(path.join(frontendPath, 'css')));
-app.use('/js', express.static(path.join(frontendPath, 'js')));
-app.use('/image', express.static(path.join(frontendPath, 'image')));
-app.use('/Image', express.static(path.join(frontendPath, 'Image')));
+app.use('/HTML', express.static(path.join(frontendPath, 'HTML')));
+app.use('/JS', express.static(path.join(frontendPath, 'JS')));
+app.use('/js', express.static(path.join(frontendPath, 'JS')));
 
 const carsRouter = require('./routes/carRoutes');
 const adminRouter = require('./routes/adminRoutes');
-const userRouter = require('./routes/userRoutes');  
+const userRouter = require('./routes/userRoutes');
 
 app.use('/api', carsRouter);
 app.use('/api/admin', adminRouter);
-app.use('/api/users', userRouter);  
+app.use('/api/users', userRouter);
 
 app.use('/api', (req, res) => {
-    res.status(404).json({ message: 'Маршрут не найден' });
+    res.status(404).json({ message: 'API Route Not Found' });
 });
 
 app.get('*', (req, res) => {
@@ -33,5 +25,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Сервер успешно запущен на порту ${PORT}`);
-});
+    console.log(`Server successfully running on port ${PORT}`);
+});ы
